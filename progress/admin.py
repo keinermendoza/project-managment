3e5cebfaf153b5d202b703925b232b0447cdc729
keinermendoza <keinermendoza@gmail.com>
2023-12-13 16:31:12 -0400

    registring taks on admin

--- progress/admin.py
+++ progress/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Task
+
+class TaskAdmin(admin.StackedInline):
+    model = Task
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['name', 'importance', 'status', 'created']
     search_fields = ['name', 'description']
-
+    inlines = [TaskAdmin]
