97a82d7b2e6efb241dec16e3d32b2beb14edd369
keinermendoza <keinermendoza@gmail.com>
2023-12-13 13:17:39 -0400

    creating model for tasks

--- progress/models.py
+++ progress/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    class Importance(models.IntegerChoices):
+        LOW = (1, 'Low')
+        INTERESTIG = (2, 'Interesting')
+        GOOD = (3, 'Good')
+        IMPORTANT = (4, 'Importat')
+        URGENT = (5, 'Urgent')       
+
+    class Status(models.IntegerChoices):
+        IDEA = (1, 'Proccessing the business model')
+        DESING = (2, 'Desiging the user interaction')
+        MODELS = (3, 'Creating prototipe app')
+        GROW = (4, 'Adjusting details')
+        IMPLEMENTIG = (5, 'Deploying version 1')
+        ITERATING = (6, 'Improving Performance and adding new Features')
+
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    importance = models.IntegerField(choices=Importance.choices, default=Importance.GOOD)
+    status = models.IntegerField(choices=Status.choices, default=Status.IDEA)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-importance', '-status', '-created']
+        indexes = [models.Index(
+            fields=['-importance', '-status', '-created']
+        )]
+
+    def __str__(self):
+        return f'Proyect: {self.name}'
+    
+
+class Task(models.Model):
+    class Status(models.IntegerChoices):
+        PENDING = (1, 'Not started yet')
+        WORKING = (2, 'Working on it')
+        WAITING = (3, 'Waiting feed back')
+        FINISHED = (4, 'Completed')
+    
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    status = models.IntegerField(choices=Status.choices, default=Status.PENDING)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created', '-status',]
+        indexes = [models.Index(
+            fields=['-created', '-status']
+        )]
+
+    def __str__(self):
+        return f'Task: {self.name} from {self.project}'
